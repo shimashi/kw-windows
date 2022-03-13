e8ae3a560311b6f6dab45586425f923569e10bc8
shimashi <shimashi@gmail.com>
2022-03-13 16:51:13 -0400

    Update winupdate.cmd

--- winupdate.cmd
+++ winupdate.cmd
@@ -1,2 +1,9 @@
+
 :: Windows update
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "& './msupdate.ps1'"
+echo Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force > c:\msupdate.ps1
+echo Install-Module PSwindowsUpdate -Force >>  c:\msupdate.ps1
+echo Install-WindowsUpdate -MicrosoftUpdate  -Verbose -AcceptAll -AutoReboot >> c:\msupdate.ps1
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "& 'c:\msupdate.ps1'"
+type %%tmp%%\msupdate.ps1
+pause
