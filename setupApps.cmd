a5eec4df67303813fd7ea5a4483a63ca5efdaaf6
shimashi@gmail.com <shimashi@gmail.com>
2022-04-09 15:01:10 -0400

    add systeminfo report script

--- setupApps.cmd
+++ setupApps.cmd
@@ -1,4 +1,5 @@
 @echo off
+SET mypath=%~dp0
 @powershell -NoProfile -ExecutionPolicy Bypass "((New-Object System.Net.WebClient).DownloadFile(' https://cdn.novabench.com/novabench.msi', 'c:\novabench.msi'))"
 
 :: Install choco and add choco to path
@@ -6,10 +7,12 @@
 :: Install packages
 FOR %%G IN (chocolatey, brave, bitwarden, zoom, 7zip, Audacity, firefox, gimp, handbrake, inkscape, krita, libreoffice, adobereader, tux-paint, vlc, googlechrome ) DO (choco upgrade %%G -fy)
 
-MsiExec.exe /i 	c:\novabench.msi /qn
+MsiExec.exe /i 	c:\novabench.msi /qnA
+cd %mypath%
 del /Q  %public%\desktop\*
 xcopy /E /Y ".\folders\*"  %public%\desktop\
 
 mkdir c:\kindworks
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "powercfg /batteryreport /output 'C:\kindworks\battery-report.html'"
+systeminfo > "c:\kindworks\system_info.txt"
 @pause 
