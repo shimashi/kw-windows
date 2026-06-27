64364c48999c53215445d453f42ee6066bb5e094
Git Duder <64866575+gitduder@users.noreply.github.com>
2026-06-27 18:24:59 -0400

    Update Initial File Copy Script.cmd
    Modified so that you download the PowerShell scripts from Github, not an external thumb drive.  This will likely require additional testing.

--- PowerShellScriptsAdam/Initial File Copy Script.cmd
+++ PowerShellScriptsAdam/Initial File Copy Script.cmd
@@ -1,11 +1,13 @@
 @echo off
+setlocal
 
 Echo.
 Echo Begin Initial File Copy Script
 Echo.
 
-REM This script creates a folder and then copies all necessary files onto the computer to be refurbished to prepare it to run the initialization scripts.
-REM Run this script first, after the refurbished computer has had a fresh OS installed.  Internet connectivity is not necessary for this script, but is needed for subsequent scripts
+REM This script creates a folder called "C:\KindWorksScripts" and then downloads all necessary files onto the computer to be refurbished to prepare it to run the initialization scripts.
+REM This script downloads the necessary install scripts from the https://github.com/shimashi/kw-windows/ github site
+REM Run this script first, after the refurbished computer has had a fresh OS installed.  Internet connectivity is necessary for this script
 REM This script must be saved in UTF-8 mode, not "UTF-8 wth BOM"
 REM When you save the file in notepad, make sure the "Encoding" option at the bottom of the "Save As" menu has UTF-8 selected
 REM Otherwise it will display every single line in the terminal which will not look good.
@@ -13,149 +15,38 @@ REM Otherwise it will display every single line in the terminal which will not l
 echo Checking to see if KindWorksScripts folder exists
 echo.
 
-IF EXIST "C:\KindWorksScripts" 
-	(
-		echo KindWorksScripts folder already exists
-		echo.
-    )
-ELSE 
-	(
-		echo KindWorksScripts folder does not exist.  Creating one now.
-		mkdir "C:\KindWorksScripts"
-		echo KindsWorksScripts folder sucessfully created under C:
-		echo. 
-	)
+REM --- Create Paths ---
+set ZIPFILE=%USERPROFILE%\Desktop\kw-windows.zip
+set EXTRACTDIR=%USERPROFILE%\Desktop\AllExtractedScripts
+set TARGETDIR=C:\KindWorksScripts
 
+REM --- Create a folder for the extracted zip files if needed ---
+if not exist "%EXTRACTDIR%" (
+    mkdir "%EXTRACTDIR%"
+)
 
-echo Listing all available drives that you can copy files from:
-echo.
+REM --- Extract ZIP into AllExtractedScripts ---
+powershell -command "Expand-Archive -Path '%ZIPFILE%' -DestinationPath '%EXTRACTDIR%' -Force"
 
-powershell -command "Get-PSDrive -PSProvider 'FileSystem' | Where-Object { $_.Name -ne 'C' } | ForEach-Object { $_.Name + ':' }"
-echo.
-    
-REM Ask the user to type in the drive letter where the KindWorks files reside
-setlocal enabledelayedexpansion
-choice /c ABCDEFGHIJKLMNOPQRSTUVWXYZ /n /m "Enter the drive letter where the KindWorks installation files reside:"
-set "letter=%errorlevel%"
-echo.
-REM echo letter is now: !letter! 
+REM --- GitHub ZIPs extract into kw-windows-main ---
+set REPOFOLDER=%EXTRACTDIR%\kw-windows-main
+set SOURCEFOLDER=%REPOFOLDER%\PowerShellScriptsAdam
 
-    
-REM Convert the key that the user just pressed into a variable called "selected_drive"
-set "letters=ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-set /a index=!letter! - 1
-set "selected_drive=!letters:~%index%,1!"
+REM --- Create target folder if needed ---
+if not exist "%TARGETDIR%" (
+    mkdir "%TARGETDIR%"
+)
 
-echo You selected the following drive letter: !selected_drive!     
-echo.
-    
-
-REM Start copying the five files needed to initialize the KindWorks computers
-REM The source folder is <mapped drive letter>:\KindWorksScripts
-REM The destination folder is C:\KindWorksScripts
-REM After each copy, check to make sure the file exists in the destination.
-
-REM Set a variable to count the number of files properly copied.  Increment after each sucessfull copied file.
-set "success_count=0"
-
-set "file1=%selected_drive%:\KindWorksScripts\Bootup Bypass Script.cmd"
-REM echo file1 is now %file1%
-echo.
-Copy "%file1%" "C:\KindWorksScripts\"
-echo.
-IF EXIST "C:\KindWorksScripts\Bootup Bypass Script.cmd" 
-	(
-		echo %file1% copied sucessfully to C drive
-		set /a success_count+=1
-		echo.
-	)
-ELSE
-	(
-		echo %file1% was NOT copied sucessfully to C drive.
-		echo.
-	)
-
-
-set "file2=%selected_drive%:\KindWorksScripts\Initialize Script.ps1"
-REM echo file2 is now %file1%
-echo.
-Copy "%file2%" "C:\KindWorksScripts\"
-echo.
-IF EXIST "C:\KindWorksScripts\Initialize Script.ps1"
-	(
-		echo %file2% copied sucessfully to C drive
-		set /a success_count+=1
-		echo.
-    )
-ELSE
-	(
-		echo %file2% was NOT copied sucessfully to C drive.
-		echo.
-	)
-
-
-set "file3=%selected_drive%:\KindWorksScripts\Update Apps.cmd"
-REM echo file3 is now %file3%
-echo.
-Copy "%file3%" "C:\KindWorksScripts\"
-echo.
-IF EXIST "C:\KindWorksScripts\Update Apps.cmd"
-	(
-		echo %file3% copied sucessfully to C drive
-		set /a success_count+=1
-		echo.
-    )
-ELSE
-	(
-		echo %file3% was NOT copied sucessfully to C drive.
-		echo.
-	)
-
-
-set "file4=%selected_drive%:\KindWorksScripts\Update Apps.ps1"
-REM echo file4 is now %file4%
-echo.
-Copy "%file4%" "C:\KindWorksScripts\"
-echo.
-IF EXIST "C:\KindWorksScripts\Update Apps.ps1"
-	(
-		echo %file4% copied sucessfully to C drive
-		set /a success_count+=1
-		echo.
-    )
-ELSE
-	(
-		echo %file4% was NOT copied sucessfully to C drive.
-		echo.
-	)
-
-
-set "file5=%selected_drive%:\KindWorksScripts\KindWorks.pow"
-REM echo file5 is now %file5%
-echo.
-Copy "%file5%" "C:\KindWorksScripts\"
-echo.
-IF EXIST "C:\KindWorksScripts\KindWorks.pow"
-	(
-		echo %file5% copied sucessfully to C drive
-		set /a success_count+=1
-		echo.
-    )
-ELSE
-	(
-		echo %file5% was NOT copied sucessfully to C drive.
-		echo.
-	)
-
-REM At the end, before pause:
-echo.
-echo ========================================
-echo Copy Summary:
-echo ========================================
-echo Successfully copied: %success_count% file(s)
-echo ========================================
+REM --- Move files ---
+if exist "%SOURCEFOLDER%" (
+    move "%SOURCEFOLDER%\*" "%TARGETDIR%"
+    echo Files moved successfully.
+) else (
+    echo ERROR: Folder "%SOURCEFOLDER%" not found.
+)
 
 endlocal
+
 
 start "" "C:\KindWorksScripts\"
 
