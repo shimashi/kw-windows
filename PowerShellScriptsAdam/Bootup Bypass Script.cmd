424adb471011d20454765844281fd7af38ce8a44
Git Duder <64866575+gitduder@users.noreply.github.com>
2026-06-28 10:35:43 -0400

    Added additional screen text
    Added some additional messages to make it easier for users to see what was happening.
    
    Removed some old REM'd out code to clean it up.

--- PowerShellScriptsAdam/Bootup Bypass Script.cmd
+++ PowerShellScriptsAdam/Bootup Bypass Script.cmd
@@ -4,18 +4,22 @@ REM This script must be saved in UTF-8 mode, not "UTF-8 wth BOM"
 REM When you save the file in notepad, make sure the "Encoding" option at the bottom of the "Save As" menu has UTF-8 selected
 REM Otherwise it will display every single line in the terminal which will not look good.
 
-Echo This script must be run at startup to allow unsigned powershell scripts to be run
+
+Echo BEGIN THE BOOTUP BYPASS SCRIPT
+Echo.
+Echo This script allows unsigned powershell scripts to be run
 Echo.
 Echo The upcoming command temporarily changes the execution policy to "bypass" for the "C:\KindWorksScripts\Initialize Script.ps1" file to run this unsigned PowerShell script
+Echo.
 Echo The upcoming command also runs the PowerShell script "C:\KindWorksScripts\Initialize Script.ps1" to configure first-time-use settings
 Echo. 
-
-REM OLD delete me - powershell -ExecutionPolicy Bypass -File "C:\KindWorksScripts\Initialize Script.ps1"
 
 powershell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy','Bypass','-File','\"C:\KindWorksScripts\Initialize Script.ps1"' -Verb RunAs"
 
 @echo off
-Echo Press any key to close the window of the Bootup Bypass Script
+Echo The Bootup Bypass Script is complete
+Echo.
+Echo Press any key to close the window
 
 @echo off
 pause >nul
